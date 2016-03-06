29c054759b72a697199ff77faf39f2918983ed9b
Tanay Choudhary <tanay@u.northwestern.edu>
2016-03-06 17:11:58 -0600

    EigenFace recognizer almost done

--- Face Recognition/gui_face.py
+++ Face Recognition/gui_face.py
@@ -7,18 +7,18 @@ svalue = StringVar() # defines the widget state as string
 w = Entry(root,textvariable=svalue) # adds a textarea widget
 w.pack()
 
+
 def train_btn_load():
     name = svalue.get()
-    os.system('python trainEigenFaces.py %s'%name)
+    os.system('python face_train_eigen.py %s'%name)
 
-# def recog_btn_load():
-#     name = svalue.get()
-#     os.system('python face_recog.py')
-    
+def recog_btn_load():
+    os.system('python face_recog_eigen.py')
+
 train_btn = Button(root,text="Train EigenFaces", command=train_btn_load)
 train_btn.pack()
 
-# recog_btn = Button(root,text="Recognize", command=recog_btn_load)
-# recog_btn.pack()
+recog_btn = Button(root,text="Recognize", command=recog_btn_load)
+recog_btn.pack()
 
 root.mainloop()
